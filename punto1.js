6d7b5ef60ac13e45268bd6e0bd81567968c3de8b
Ronald Carrillo <ronaldcarrillogarvin@gmail.com>
2025-05-30 08:08:50 -0500

    Primer punto..

---
+++ punto1.js
@@ -0,0 +1,12 @@
+// PUNTO 1
+let num1 = 10;
+let num2 = 5;
+let suma = num1 + num2
+let resta = num1 - num2
+let multiplicacion = num1 * num2
+let division = num1 / num2
+console.log("El resultado de la suma es: "+suma);
+console.log("La diferencia de la resta es: "+resta);
+console.log("El producto de la multiplicación es: "+multiplicacion);
+console.log("El cociente de la división es: "+division);
+
